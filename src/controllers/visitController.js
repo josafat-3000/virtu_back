b70fc2bb78f3d7c5edc376303097672fa402bb81
josafat <josafat30000@gmail.com>
2025-02-17 16:39:46 -0600

    17/02

--- src/controllers/visitController.js
+++ src/controllers/visitController.js
@@ -1,5 +1,5 @@
 import prisma from '@prisma/client';
-import {sendEmail} from '../utils/email.js';
+import { sendEmail } from '../utils/email.js';
 import { sendNotificationToUser } from './notificationController.js';
 
 const { PrismaClient } = prisma;
@@ -71,11 +71,11 @@ export const updateVisitStatus = async (req, res) => {
         const visit = await prismaClient.visits.findUnique({
             where: { id: parseInt(id, 10) },
         });
-        
+
         const remitente = await prismaClient.users.findUnique({
             where: { id: parseInt(visit.user_id, 10) }
         })
-       
+
 
         if (!visit) {
             return res.status(404).send({ error: 'Visit not found' });
@@ -100,31 +100,82 @@ export const updateVisitStatus = async (req, res) => {
         if (newStatus === 'completed') {
             const message = `La visita con ID ${id} ha sido completada.`;
             await prismaClient.notifications.create({
-              data: { visit_id: visit.id, user_id: visit.user_id, notification_type: 'check_out' },
+                data: { visit_id: visit.id, user_id: visit.user_id, notification_type: 'check_out' },
             });
-      
+
             // Enviar la notificación a través de SSE
             sendNotificationToUser(remitente.id, message);
-          } else if (newStatus === 'in_progress') {
+        } else if (newStatus === 'in_progress') {
             const message = `La visita con ID ${id} ha ingresado.`;
             await prismaClient.notifications.create({
-              data: { visit_id: visit.id, user_id: visit.user_id, notification_type: 'check_in' },
+                data: { visit_id: visit.id, user_id: visit.user_id, notification_type: 'check_in' },
             });
-      
+
             // Enviar la notificación a través de SSE
             sendNotificationToUser(remitente.id, message);
-          }
+        }
 
-        
+
         const subject = `Notificación de Visita `;
-        const text = `La visita con ID ${visit.id} ha registrado el evento: ${newStatus}.`;
         const html = `
-        <p><strong>Notificación de Visita</strong></p>
-        <p>La visita con ID <strong>${visit.id}</strong> ha registrado el evento: <strong>${newStatus}</strong>.</p>
-        `;
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Notificación de Visita</title>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              background-color: #f4f4f4;
+              margin: 0;
+              padding: 20px;
+            }
+            .container {
+              max-width: 600px;
+              margin: 0 auto;
+              background: #ffffff;
+              border-radius: 8px;
+              box-shadow: 0 0 10px rgba(0,0,0,0.1);
+              text-align: center;
+              padding: 20px;
+            }
+            .header {
+              background-color:  #00497E;;
+              color: #fff;
+              padding: 15px;
+              border-top-left-radius: 8px;
+              border-top-right-radius: 8px;
+              font-size: 20px;
+            }
+            .content {
+              font-size: 16px;
+              color: #333;
+              margin: 20px 0;
+            }
+            .footer {
+              font-size: 14px;
+              color: #777;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <div class="header">Notificación de Visita</div>
+            <div class="content">
+              <p>La visita con ID <strong>${visit.id}</strong> ha registrado el evento: <strong>${newStatus}</strong>.</p>
+            </div>
+            <div class="footer">
+              <p>&copy; ${new Date().getFullYear()} Virtu</p>
+            </div>
+          </div>
+        </body>
+        </html>
+      `;
+
 
         const recipientEmail = remitente.email; // Cambia a la dirección del destinatario
-        sendEmail(recipientEmail, subject, text, html);
+        sendEmail(recipientEmail, subject, html);
         res.send(updatedVisit);
     } catch (error) {
         console.error(error);
--- src/utils/email.js
+++ src/utils/email.js
@@ -27,27 +27,152 @@ export const sendEmail = async (to, subject, html) => {
 
 export const confirmTemplate = (name, token) => {
   return `
-        <div id="email___content">
-            
-            <h2>Hola ${name}</h2>
-            <p>Para confirmar tu cuenta y crear tu contraseña, ingresa al siguiente enlace</p>
-            <a
-                href="${process.env.FRONTEND_URL}/confirm/${token}"
-                target="_blank"
-            >Confirmar Cuenta</a>
-        </div>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Confirmación de Cuenta</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    margin: 0;
+                    padding: 0;
+                }
+                .container {
+                    max-width: 600px;
+                    margin: 20px auto;
+                    background: #ffffff;
+                    padding: 20px;
+                    border-radius: 8px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    text-align: center;
+                }
+                .header {
+                    background:rgb(0,73,126);
+                    color: white;
+                    padding: 15px;
+                    font-size: 20px;
+                    border-top-left-radius: 8px;
+                    border-top-right-radius: 8px;
+                }
+                .content {
+                    font-size: 16px;
+                    color: #333333;
+                    line-height: 1.6;
+                    padding: 20px;
+                }
+                .button {
+                    display: inline-block;
+                    background: #00497E;
+                    color: white;
+                    padding: 12px 20px;
+                    text-decoration: none;
+                    border-radius: 5px;
+                    margin-top: 20px;
+                    font-size: 16px;
+                }
+                .footer {
+                    text-align: center;
+                    padding: 15px;
+                    font-size: 14px;
+                    color: #777777;
+                }
+                a{
+                color:rgb(255, 255, 255) !important;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="header">Confirmación de Cuenta</div>
+                <div class="content">
+                    <h2>Hola ${name},</h2>
+                    <p>Para confirmar tu cuenta y crear tu contraseña, haz clic en el siguiente botón:</p>
+                    <a href="${process.env.FRONTEND_URL}/confirm/${token}" target="_blank" class="button">Confirmar Cuenta</a>
+                </div>
+                <div class="footer">
+                    <p>&copy; ${new Date().getFullYear()} Virtu</p>
+                </div>
+            </div>
+        </body>
+        </html>
       `;
 }
+
 export const forgotTemplate = (name, token) => {
   return `
-        <div id="email___content">
-            
-            <h2>Hola ${name}</h2>
-            <p>Para cambiar tu contraseña, ingresa al siguiente enlace</p>
-            <a
-                href="${process.env.FRONTEND_URL}/reset/${token}"
-                target="_blank"
-            >Cambiar contraseña</a>
-        </div>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Restablecer Contraseña</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    margin: 0;
+                    padding: 0;
+                }
+                .container {
+                    max-width: 600px;
+                    margin: 20px auto;
+                    background: #ffffff;
+                    padding: 20px;
+                    border-radius: 8px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    text-align: center;
+                }
+                .header {
+                    background:rgb(0,73,126);
+                    color: white;
+                    padding: 15px;
+                    font-size: 20px;
+                    border-top-left-radius: 8px;
+                    border-top-right-radius: 8px;
+                }
+                .content {
+                    font-size: 16px;
+                    color: #333333;
+                    line-height: 1.6;
+                    padding: 20px;
+                }
+                .button {
+                    display: inline-block;
+                    background: #00497E;
+                    color: white;
+                    padding: 12px 20px;
+                    text-decoration: none;
+                    border-radius: 5px;
+                    margin-top: 20px;
+                    font-size: 16px;
+                }
+                .footer {
+                    text-align: center;
+                    padding: 15px;
+                    font-size: 14px;
+                    color: #777777;
+                }
+                a{
+                color:rgb(255, 255, 255) !important;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="header">Restablecer Contraseña</div>
+                <div class="content">
+                    <h2>Hola ${name},</h2>
+                    <p>Para cambiar tu contraseña, haz clic en el siguiente botón:</p>
+                    <a href="${process.env.FRONTEND_URL}/reset/${token}" target="_blank" class="button">Cambiar Contraseña</a>
+                </div>
+                <div class="footer">
+                    <p>&copy; ${new Date().getFullYear()} Vrtu.</p>
+                </div>
+            </div>
+        </body>
+        </html>
       `;
 }
